24dbd5097ea30220d70df0ee1e30cea6e5530289
Nathaniel Sena <nate.sena1@gmail.com>
2017-11-06 13:38:04 -0800

    halfway through login

---
+++ index.vr.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  Pano,
+  Text,
+  View,
+  VrButton,
+} from 'react-vr';
+import TextInput from './js/vr_components/textInput.js'
+
+export default class test_vr extends React.Component {
+ 
+  clickity(){
+    console.log('clicked')
+    this.setState({
+      clicked: !this.state.clicked
+    })
+  }
+  type(evt){
+    console.log(evt)
+  }
+  render() {
+    return (
+      <View onInput={(evt)=>{
+        if(evt.nativeEvent.inputEvent.type == 'KeyboardInputEvent'){
+          this.type(evt)
+        }
+      }}>
+
+        <Pano source={asset('chess-world.jpg')}/>
+        
+        <TextInput placeHolder={'name'} focused={false} ></TextInput>
+        <TextInput placeHolder={'email'} focused={false} ></TextInput>
+        <TextInput placeHolder={'password'} focused={false} ></TextInput>
+        <TextInput placeHolder={'confirm password'} focused={false} ></TextInput>
+        <VrButton onClick={this.clickity.bind(this)}>
+          <Text
+            style={{
+              backgroundColor: '#777879',
+              fontSize: 0.2,
+              fontWeight: '400',
+              layoutOrigin: [0.5, 0.5],
+              paddingLeft: 0.2,
+              paddingRight: 0.2,
+              textAlign: 'center',
+              textAlignVertical: 'center',
+              transform: [{translate: [0, 0, -3]}],
+            }}>
+            Submit
+          </Text>
+        </VrButton>
+       
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('test_vr', () => test_vr);
---
+++ js/vr_components/textInput.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import {
+    AppRegistry,
+    Text,
+    View,
+    VrButton,
+  } from 'react-vr';
+
+  export default class TextInput extends React.Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            text: props.placeHolder,
+            fontWeight: 400,
+            color: '#FF0000',
+            focused: props.focused,
+            typed: false
+        }
+    }
+
+    focus(){
+      this.setState({
+        color: this.state.focused? '#FF0000': '#00FF00',
+        focused: !this.state.focused,
+        fontWeight: 100,
+      }, ()=>{
+        console.log('placeholder clicked', this.state.focused)
+      })
+    }
+  
+    type(evt){
+      console.log(evt.nativeEvent.inputEvent.type)
+    }
+    keyPressed(){
+
+    }
+    render() {
+      return (
+        <View>
+          <VrButton onEnter={this.focus.bind(this)} onExit={this.focus.bind(this)} onInput={(evt)=>this.type(evt)}onKeyDown={this.type.bind(this)}>
+            <Text 
+              style={{
+                margin: 0.05,
+                backgroundColor: `${this.state.color}`,
+                fontSize: 0.2,
+                fontWeight: `${this.state.fontWeight}`,
+                layoutOrigin: [0.5, 0.5],
+                paddingLeft: 0.2,
+                paddingRight: 0.2,
+                textAlign: 'center',
+                textAlignVertical: 'center',
+                transform: [{translate: [0, 0, -3]}],
+              }}>
+              {this.state.text}
+            </Text>
+          </VrButton>
+        </View>
+      );
+    }
+  };
+  
+  AppRegistry.registerComponent('textInput', () => textInput);
